5211a87bdfc0b34763451f1be6ba94ce29b8856e
unknown <Hegyi Gbor@NET1.LOCAL>
2025-11-13 11:22:02 +0100

    P hozzáadás

--- dom.js
+++ dom.js
@@ -6,12 +6,23 @@ function addDiv() {
     const h2 = document.createElement('h2');
     h2.innerHTML = 'Cikk ' + szamlalo;
     ujDiv.append(h2);
-    
+
     const container = document.getElementById('container');
     container.append(ujDiv);
 
     szamlalo++;
 }
 
-
+function addP() {
+    const ujP = document.createElement('p');
+    ujP.innerHTML = 'Új P';
+    const container = document.getElementById('container');
+    const divs = container.getElementsByTagName('div');
+    const lastDiv = divs[divs.length - 1];
+    if (divs.length > 0) {
+        lastDiv.append(ujP);
+    } else {
+        alert('Előbb divet hozz létre!!!!!!!!!!!!!!!');
+    }
+}
 
