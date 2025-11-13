1877aef7e73a2c945a7cf85cb5c0e83aad1b4e07
unknown <Hegyi Gbor@NET1.LOCAL>
2025-11-13 12:17:27 +0100

    stilusgomb

--- dom.js
+++ dom.js
@@ -22,7 +22,33 @@ function addP() {
     if (divs.length > 0) {
         lastDiv.append(ujP);
     } else {
-        alert('Előbb divet hozz létre!!!!!!!!!!!!!!!');
+        alert('Előbb divet hozz létre!!!!!!!!!!!!!!!!!!!!!!!');
     }
 }
 
+function UseStilus() {
+    const bekezdesek1 = document.querySelectorAll('div p');
+    bekezdesek1.forEach(function (p) {
+        p.style.color = 'blue';
+    });
+
+    const bekezdesek2 = document.querySelectorAll('div > p');
+    bekezdesek2.forEach(function (p) {
+        p.style.fontWeight = 'bold';
+    });
+
+    const bekezdesek3 = document.querySelectorAll('div ~ p');
+    bekezdesek3.forEach(function (p) {
+        p.style.textDecoration = 'underline';
+    });
+
+    const bekezdesek4 = document.querySelectorAll('div p:nth-child(3)');
+    bekezdesek4.forEach(function (p) {
+        p.style.backgroundColor = 'Lightyellow';
+    });
+
+    const bekezdesek5 = document.querySelectorAll('p:nth-of-type(2)');
+    bekezdesek5.forEach(function (p) {
+        p.style.border = '1px solid red';
+    });
+}   
